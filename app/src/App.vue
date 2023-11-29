34fbe125e1cd1c32b581dc48e7a9be6336558ebc
JHOLLID2 <jhollid2@jaguarlandrover.com>
2023-11-29 09:00:25 +0000

    Added button to toggle between bookings and rooms

--- app/src/App.vue
+++ app/src/App.vue
@@ -2,13 +2,15 @@
   <v-app class="app-root">
     <v-app-bar app color="primary" dark>
       <v-app-bar-title>Vanti Lead Dev Assessment</v-app-bar-title>
+      <v-spacer/>
+      <v-btn @click="roomToggle = !roomToggle;">Show {{roomToggle ? 'bookings' : 'rooms'}}</v-btn>
     </v-app-bar>
 
     <v-main>
       <v-container>
         <v-card>
-          <bookings-table/>
-          <rooms-table/>
+          <rooms-table v-if="roomToggle === true"/>
+          <bookings-table v-else/>
         </v-card>
       </v-container>
     </v-main>
@@ -18,4 +20,15 @@
 <script setup>
 import BookingsTable from '@/components/BookingsTable.vue';
 import RoomsTable from '@/components/RoomsTable.vue';
+</script>
+
+<script>
+export default {
+  data() {
+    return {
+      roomToggle: false,
+      btnValue: 'show rooms',
+    };
+  },
+}
 </script>
